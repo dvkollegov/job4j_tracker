c3a10192d4dac7ca78ad75587e045417093beb67
Dima <dvkollegov@mail.ru>
2022-11-22 15:48:18 +0300

    8. Date. Отображение даты. [#310017 #363176]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,4 +8,5 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(formatter));
     }
+
 }
