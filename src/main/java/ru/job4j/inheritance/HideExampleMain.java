345ea265421dcd84e72505d13aeaed114700d522
Dima <dvkollegov@mail.ru>
2022-11-22 14:12:47 +0300

    5. Сокрытие метода [#504921] // статический (скрытый) метод

---
+++ src/main/java/ru/job4j/inheritance/HideExampleMain.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class HideExampleMain {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal cat = new Cat();
+        animal.instanceInvoke();
+        cat.instanceInvoke();
+        Animal.staticInvoke();
+        Cat.staticInvoke();
+        Animal dog = new Dog();
+        dog.instanceInvoke();
+        Dog.staticInvoke();
+    }
+}
