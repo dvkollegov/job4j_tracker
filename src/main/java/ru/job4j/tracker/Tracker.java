e67d7edac6595740788101820e80e930f4beab36
Dima <dvkollegov@mail.ru>
2022-11-24 12:39:32 +0300

    8. Что такое валидация? [#246864 #363761] // переменная для if() проверяемого условия

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,22 +47,22 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rls = index != -1;
+        if (rls) {
             item.setId(id);
             items[index] = item;
-            return true;
         }
-        return false;
+        return rls;
     }
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rls = index != -1;
+        if (rls) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return true;
         }
-        return false;
+        return rls;
     }
 }
