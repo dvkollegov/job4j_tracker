3e49898ac3f5305a8cff4dba4824124484c7aae7
Dima <dvkollegov@mail.ru>
2022-11-25 15:04:48 +0300

    9. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,4 +4,52 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateItem(),
+                new ExitProgram()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        /* ƒобавим в tracker новую за€вку */
+        Item item = tracker.add(new Item("Replaced item"));
+        /* ¬ходные данные должны содержать ID добавленной за€вки item.getId() */
+        String replacedName = "New item name";
+        String id = String.valueOf(item.getId());
+        Input in = new StubInput(
+                new String[] {"0", id, replacedName, "1"}
+        );
+        UserAction[] actions = {
+                new EditItem(),
+                new ExitProgram()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Deleted item"));
+        String id = String.valueOf(item.getId());
+        Input in = new StubInput(
+                new String[] {"0", id, "1"}
+        );
+        UserAction[] actions = {
+                new DeleteItem(),
+                new ExitProgram()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId())).isNull();
+    }
 }
