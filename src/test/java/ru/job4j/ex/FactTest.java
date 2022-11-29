b9546d059601ca9e47849ce3b1875624a8583923
Dima <dvkollegov@mail.ru>
2022-11-29 23:53:13 +0300

    0.3. Тестирование исключений с junit [#219366 #365497]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -15,4 +15,12 @@ class FactTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
     }
+
+    @Test
+    public void whenRight() {
+        int value = 5;
+        int expected = 120;
+        int result = Fact.calc(value);
+        assertThat(result).isEqualTo(expected);
+    }
 }
