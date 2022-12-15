9dc9f1f2977b323f4051e29b27b37fda1116f34c
Dima <dvkollegov@mail.ru>
2022-12-15 08:35:09 +0300

    4. Аттестация [#504878 #371278] // меньше переменных

--- src/main/java/ru/job4j/map/AnalyzeByMap.java
+++ src/main/java/ru/job4j/map/AnalyzeByMap.java
@@ -7,9 +7,8 @@ public class AnalyzeByMap {
         double rsl = 0D;
         int count = 0;
         for (Pupil pupil : pupils) {
-            List<Subject> subjects = pupil.subjects();
             count += pupil.subjects().size();
-            for (Subject subject : subjects) {
+            for (Subject subject : pupil.subjects()) {
                 rsl += subject.score();
             }
         }
@@ -19,14 +18,11 @@ public class AnalyzeByMap {
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
         List<Label> rsl = new ArrayList<>();
         for (Pupil pupil : pupils) {
-            String name = pupil.name();
             double sum = 0D;
-            int count = pupil.subjects().size();
-            List<Subject> subjects = pupil.subjects();
-            for (Subject subject : subjects) {
+            for (Subject subject : pupil.subjects()) {
                 sum += subject.score();
             }
-            Label label = new Label(name, sum / count);
+            Label label = new Label(pupil.name(), sum / pupil.subjects().size());
             rsl.add(label);
         }
         return rsl;
@@ -36,11 +32,9 @@ public class AnalyzeByMap {
         List<Label> rsl = new ArrayList<>();
         LinkedHashMap<String, Integer> temp = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
-            List<Subject> subjects = pupil.subjects();
-            for (Subject subject : subjects) {
-                String name = subject.name();
-                int score = temp.getOrDefault(name, 0);
-                temp.put(name, score + subject.score());
+            for (Subject subject : pupil.subjects()) {
+                int score = temp.getOrDefault(subject.name(), 0);
+                temp.put(subject.name(), score + subject.score());
             }
         }
         for (Map.Entry<String, Integer> entry : temp.entrySet()) {
@@ -54,13 +48,11 @@ public class AnalyzeByMap {
     public static Label bestStudent(List<Pupil> pupils) {
         List<Label> list = new ArrayList<>();
         for (Pupil pupil : pupils) {
-            String name = pupil.name();
             double sum = 0D;
-            List<Subject> subjects = pupil.subjects();
-            for (Subject subject : subjects) {
+            for (Subject subject : pupil.subjects()) {
                 sum += subject.score();
             }
-            Label label = new Label(name, sum);
+            Label label = new Label(pupil.name(), sum);
             list.add(label);
         }
         list.sort(Comparator.naturalOrder());
@@ -71,11 +63,9 @@ public class AnalyzeByMap {
         List<Label> list = new ArrayList<>();
         LinkedHashMap<String, Integer> temp = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
-            List<Subject> subjects = pupil.subjects();
-            for (Subject subject : subjects) {
-                String name = subject.name();
-                int score = temp.getOrDefault(name, 0);
-                temp.put(name, score + subject.score());
+            for (Subject subject : pupil.subjects()) {
+                int score = temp.getOrDefault(subject.name(), 0);
+                temp.put(subject.name(), score + subject.score());
             }
         }
         for (Map.Entry<String, Integer> entry : temp.entrySet()) {
