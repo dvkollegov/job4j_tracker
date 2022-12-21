be89c218fdca2bd053a841131ae93ed66e152b06
Dima <dvkollegov@mail.ru>
2022-12-21 14:56:12 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        String[] arr1 = o1.split("/");
+        String[] arr2 = o2.split("/");
+        return arr1[0].equals(arr2[0]) ? o1.compareTo(o2) : arr2[0].compareTo(arr1[0]);
+    }
+}
