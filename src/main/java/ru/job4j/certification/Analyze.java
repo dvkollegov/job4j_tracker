9e1ec24af16cae5de80a017773667cd883a910a6
Dima <dvkollegov@mail.ru>
2022-12-30 16:07:26 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/certification/Analyze.java
@@ -0,0 +1,51 @@
+package ru.job4j.certification;
+
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(ppl -> ppl.subjects().stream())
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(Double.NaN);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.map(ppl -> new Tuple(ppl.name(), ppl.subjects().stream()
+                        .mapToInt(Subject::score)
+                        .average()
+                        .orElse(Double.NaN)))
+                .toList();
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.flatMap(ppl -> ppl.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name,
+                        LinkedHashMap::new,
+                        Collectors.averagingDouble(Subject::score)))
+                .entrySet().stream()
+                .map(sbj -> new Tuple(sbj.getKey(), sbj.getValue()))
+                .toList();
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(ppl -> new Tuple(ppl.name(), ppl.subjects().stream()
+                        .mapToInt(Subject::score)
+                        .sum()))
+                .max(Comparator.comparingDouble(Tuple::score)).get();
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(ppl -> ppl.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name,
+                        LinkedHashMap::new,
+                        Collectors.summingDouble(Subject::score)))
+                .entrySet().stream()
+                .map(sbj -> new Tuple(sbj.getKey(), sbj.getValue()))
+                .max(Comparator.comparingDouble(Tuple::score)).get();
+    }
+}
---
+++ src/main/java/ru/job4j/certification/Subject.java
@@ -0,0 +1,4 @@
+package ru.job4j.certification;
+
+public record Subject(String name, int score) {
+}
