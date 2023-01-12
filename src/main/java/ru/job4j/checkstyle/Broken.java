a5926429e556f6ca595dec998254d3d1bb51c776
Dima <dvkollegov@mail.ru>
2023-01-12 14:56:36 +0300

    9. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    public String name;
+    public String surname;
+    public static final String NEW_VALUE = "";
+
+    public Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c) {
+    }
+}
